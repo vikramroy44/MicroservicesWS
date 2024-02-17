ad6f723cc134a53cdfe39b016e82bd1072c339ea
Vikram <vikramroy44@gmail.com>
2024-02-18 02:39:41 +0530

    Student Service Update
    Modified   MicroServiceWS/StudentService/pom.xml
    Modified   MicroServiceWS/StudentService/src/main/java/com/example/student/controller/StudentController.java
    Modified   MicroServiceWS/StudentService/src/main/resources/application.properties
    
    Signed-off-by: Vikram <vikramroy44@gmail.com>

--- MicroServiceWS/StudentService/src/main/java/com/example/student/controller/StudentController.java
+++ MicroServiceWS/StudentService/src/main/java/com/example/student/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +37,7 @@ public class StudentController {
 		
 		return new ResponseEntity<>(student, HttpStatus.CREATED);
 	}
-	@PostMapping()
+	@PutMapping()
 	public ResponseEntity<Student> updateStudent(@RequestBody Student st){
 		System.out.println("Update Invoked");
 		Student student = this.stService.upsertStudent(st);

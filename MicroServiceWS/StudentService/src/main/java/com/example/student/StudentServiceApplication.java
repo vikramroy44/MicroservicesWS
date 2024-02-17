d2b73891a50650bb31f79579b21f64c2c740eaa0
Vikram <vikramroy44@gmail.com>
2024-02-17 10:03:18 +0530

    Initial Student Service Project Setup
    Added      MicroServiceWS/.gitignore
    Added      MicroServiceWS/StudentService/.gitignore
    Added      MicroServiceWS/StudentService/.mvn/wrapper/maven-wrapper.jar
    Added      MicroServiceWS/StudentService/.mvn/wrapper/maven-wrapper.properties
    Added      MicroServiceWS/StudentService/mvnw
    Added      MicroServiceWS/StudentService/mvnw.cmd
    Added      MicroServiceWS/StudentService/pom.xml
    Added      MicroServiceWS/StudentService/src/main/java/com/example/student/StudentServiceApplication.java
    Added      MicroServiceWS/StudentService/src/main/java/com/example/student/controller/StudentController.java
    Added      MicroServiceWS/StudentService/src/main/resources/application.properties
    Added      MicroServiceWS/StudentService/src/test/java/com/example/student/StudentServiceApplicationTests.java
    
    Signed-off-by: Vikram <vikramroy44@gmail.com>

---
+++ MicroServiceWS/StudentService/src/main/java/com/example/student/StudentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+
+	@GetMapping("/test")
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+
+}
